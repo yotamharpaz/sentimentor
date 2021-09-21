04f22e898fb9e48cb7661b9175b9b3828e56f090
yotamharpaz <yotamh42@gmail.com>
2021-09-21 19:59:25 +0300

    colord answers

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 async function textCheck(){
+    // remove data
+    // create cool loading spinner
     const inputText = JSON.stringify(document.getElementById("textInput").value);
     const response = await fetch("https://sentim-api.herokuapp.com/api/v1/", {
         method: "POST",
@@ -9,6 +11,28 @@ async function textCheck(){
         body: JSON.stringify({ "text": inputText})
     });
     const answer = await response.json();
-    document.getElementById("polarity").textContent = answer.result.polarity;
-    document.getElementById("type").textContent = answer.result.type;
+    const textPolarityElem = document.getElementById("polarity");
+    textPolarityElem.textContent = answer.result.polarity;
+     const textTypeElem = document.getElementById("type");
+     textTypeElem.textContent = answer.result.type;
+     colorClassByPolarity(answer.result.polarity)
+}
+
+function colorClassByPolarity(polarity) {
+    const elem = document.getElementById("answer")
+    if(polarity > 0){
+        elem.classList.remove("bad");
+        elem.classList.remove("natural");
+        elem.classList.add("good")
+
+    }else if(polarity === 0) {
+        elem.classList.remove("bad");
+        elem.classList.remove("good");
+        elem.classList.add("natural");
+    }else{
+        elem.classList.remove("natural");
+        elem.classList.remove("good");
+        elem.classList.add("bad");
+    }
+
 }
